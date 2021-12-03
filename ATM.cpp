7b1cd89c139770bd6b5a52db0582035c91e72192
Bruno Felix de Sousa <brunofelixdesousa@hotmail.com>
2021-12-02 23:41:45 -0300

    adicionando classes do aplicação ATM

--- ATM.cpp
+++ ATM.cpp
@@ -1,9 +1,39 @@
-﻿// ATM.cpp: define o ponto de entrada para o aplicativo.
-//
-
+﻿
 #include "ATM.h"
+#include "Transaction.h"
+#include "BalanceInquiry.h"
+#include "Withdrawal.h"
+#include "DepositSlot.h"
 
-using namespace std;
+// constantes de enumeração representam as principais opções de menu
+enum MenuOption{ BALANCE_INQUIRY = 1, WITHDRAWAL, DEPOSIT, EXIT };
+
+// o construtor-padrão ATM inicializa os membros de dados
+ATM::ATM()
+	: userAuthenticated(false), // o usuario não foi autencticado para iniciar
+	currentAccountNumber(0)     // nenhum numero atual de conta para iniciar
+{
+	// corpo vazio
+} // fim do construtor padrao ATM
+
+void ATM::run()
+{
+	while (true)
+	{
+		while (!userAuthenticated)
+		{
+			screen.displayMessageLine("\nBem-Vindo!!!");
+			authenticateUser(); // autentica o usuario
+		}
+
+		performTransactions(); // o usuario agora está autenticado
+		userAuthenticated = false; // reinicializa antes da próxima sessão do ATM
+		currentAccountNumber = 0; // reinicializa antes da próxima sessão do ATM
+		screen.displayMessageLine("\nObrigado, Até mais...");
+	}
+} // fim do run
+
+
 
 int main()
 {
--- ATM.h
+++ ATM.h
@@ -3,6 +3,33 @@
 
 #pragma once
 
-#include <iostream>
+#include "Screen.h"
+#include "Keypad.h"
+#include "CashDispenser.h"
+#include "DepositSlot.h"
+#include "BankDatabase.h"
 
-// TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
+class Transaction;
+
+class ATM
+{
+public:
+	ATM(); // construtor inicializa membros de dados
+	void run();
+
+private:
+	bool userAuthenticated{};    // se o usuário foi autenticado
+	int currentAccountNumber{};  // número atual da conta de usuário
+	Screen screen;               // tela do ATM
+	Keypad keypad;               // teclado do ATM
+	CashDispenser cashDispenser; // dispensador de cédulas do ATM
+	DepositSlot depositSlot;     // abertura para depósito do ATM
+	BankDatabase bankDatabase;   // banco de dados com as informações sobre as conta
+
+	void authenticateUser();     // tentar autenticar o usuario
+	void performTransactions();  // realiza transações
+	int displayMainMenu() const; // exibe o menu principal
+
+	Transaction* CreateTransaction(int);
+
+};
