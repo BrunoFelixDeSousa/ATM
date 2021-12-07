1940c7b9af6b4dce0ec758666901184f1e824768
Bruno Felix de Sousa <brunofelixdesousa@hotmail.com>
2021-12-06 22:05:39 -0300

    adicionado BankDatabase.cpp

--- BankDatabase.cpp
+++ BankDatabase.cpp
@@ -6,5 +6,60 @@ BankDatabase::BankDatabase()
 	Account account1(12345, 54321, 1000.0, 1200.0);
 	Account account2(98765, 56789, 200.0, 200.0);
 
+	// adiciona os objetos Account ao vector accounts
+	accounts.push_back(account1);
+	accounts.push_back(account2);
+}
+
+Account* BankDatabase::getAccount(int accountNumber)
+{
+	for (size_t i = 0; i < accounts.size(); i++ + )
+	{
+		if (accounts[i].getAccountNumber() == accountNumber)
+		{
+			return &accounts;
+		}
+	}
+
+	return NULL;
+}
+
+bool BankDatabase::authenticateUSer(int userAccountNumber, int userPIN)
+{
+	Account* const userAccountPtr = getAccount(userPIN);
+
+	if (userAccountPtr != NULL)
+	{
+		return userAccountPtr->validatePIN(userPIN);
+	}
+	else
+	{
+		return false;
+	}
+}
+
+double BankDatabase::getAvailableBalance(int userAccountNumber)
+{
+	Account* const userAccountPtr = getAccount(userAccountNumber);
+	return userAccountPtr->getAvailableBalance();
+}
+
+double BankDatabase::getTotalBalance(int userAccountNumber)
+{
+	Account* const userAccountPtr = getAccount(userAccountNumber);
+	return userAccountPtr->getTotalBalance();
+}
+
+void BankDatabase::credit(int userAccountNumber, double amount)
+{
+	Account* const userAccountPtr = getAccount(userAccountNumber);
+	userAccountPtr->credit(amount);
+
+}
+
+void BankDatabase::debit(int userAccountNumber, double amount)
+{
+	Account* const userAccountPtr = getAccount(userAccountNumber);
+	userAccountPtr->debit(amount);
 
 }
