3cddf496747569cc3932cce4c41db609cffb7910
Bruno Felix de Sousa <brunofelixdesousa@hotmail.com>
2021-12-09 23:55:44 -0300

    adicionado BalanceInquiry.h

---
+++ BalanceInquiry.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Transaction.h"
+
+class BalanceInquiry : public Transaction
+{
+public:
+	BalanceInquiry(int, Screen&, BankDatabase&);
+	virtual void execute(); // realiza a transação
+};
