dba834d6589bc91d3e5c0b6dc211e54baeb6ae87
Bruno Felix de Sousa <brunofelixdesousa@hotmail.com>
2021-12-27 22:37:55 -0300

    atualizando bugs do arquivo ATM.cpp

--- Withdrawal.cpp
+++ Withdrawal.cpp
@@ -13,3 +13,54 @@ Withdrawal::Withdrawal(int userAccountNumber, Screen& atmScreen, BankDatabase& a
 {
 
 }
+
+// realizar transação
+void Withdrawal::execute()
+{
+	bool cashDispensed = false;
+	bool transactionCanceled = false;
+
+	BankDatabase& bankDatabase = getBankDatabase();
+	Screen& screen = getScreen();
+
+	do
+	{
+		int selection = displayMenuOfAmoount();
+		
+		if (selection != CANCELED)
+		{
+			amount = selection;
+
+			double availableBalance = bankDatabase.getAvailableBalance(getAccountNumber());
+			
+			if (amount <= availableBalance)
+			{
+				if (cashDispenser.isSufficientCashAvailable(amount))
+				{
+					bankDatabase.debit(getAccountNumber(), amount);
+
+					cashDispenser.dispenseCash(amount);
+					cashDispensed = true;
+
+					screen.displayMessageLine("\n por favor, pegue seu dinheiro no terminal");
+				}
+				else
+				{
+					screen.displayMessageLine("\nDinheiro insuficiente disponível no caixa eletronico."
+						"\npor favor escolha uma quantidade menor.");
+				}
+			}
+			else
+			{
+				screen.displayMessageLine("\nFundos insuficientes em sua conta."
+					"\npor favor escolha uma quantidade menor.");
+			}
+		}
+		else
+		{
+			screen.displayMessageLine("\ncancelando transacao...");
+			transactionCanceled = true;
+		}
+	} while (!cashDispensed && !transactionCanceled);
+}
+
